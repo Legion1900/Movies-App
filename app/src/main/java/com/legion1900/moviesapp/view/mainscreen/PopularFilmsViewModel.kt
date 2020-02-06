a609ca7b11e930d755d99f803fc6d68dfdae8fd2
Legion1900 <rom4ik730@gmail.com>
2020-02-06 13:13:25 +0200

    PopularFilmsFragment works but UI requires polishing.

--- app/src/main/java/com/legion1900/moviesapp/view/mainscreen/PopularFilmsViewModel.kt
+++ app/src/main/java/com/legion1900/moviesapp/view/mainscreen/PopularFilmsViewModel.kt
@@ -1,9 +1,44 @@
 package com.legion1900.moviesapp.view.mainscreen
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.legion1900.moviesapp.data.abs.MoviesRepository
+import com.legion1900.moviesapp.data.abs.dto.Movie
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
-class PopularFilmsViewModel @Inject constructor() : ViewModel() {
+class PopularFilmsViewModel @Inject constructor(private val repo: MoviesRepository) : ViewModel() {
+    private val disposable = CompositeDisposable()
 
+    private val movies =
+        MutableLiveData<List<Movie>>().apply { repo.loadMovies { getMovies() } }
+
+    fun getMovies(): LiveData<List<Movie>> = movies
+
+    private inline fun MoviesRepository.loadMovies(
+        request: MoviesRepository.() -> Single<List<Movie>>
+    ) {
+        disposable.addAll(
+            request().observeOn(AndroidSchedulers.mainThread())
+                .subscribe(::onSuccess, ::onError)
+        )
+    }
+
+    private fun onSuccess(movies: List<Movie>) {
+        this.movies.value = movies
+    }
+
+    private fun onError(t: Throwable) {
+        // TODO: display alert msg
+        Log.e("test", t.stackTrace.toString())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 }
---
+++ app/src/main/java/com/legion1900/moviesapp/view/mainscreen/adapters/MoviesAdapter.kt
@@ -0,0 +1,59 @@
+package com.legion1900.moviesapp.view.mainscreen.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.legion1900.moviesapp.R
+import com.legion1900.moviesapp.data.abs.dto.Movie
+
+class MoviesAdapter(
+    private val onMovieClick: (View) -> Unit,
+    private val glide: RequestManager
+) : RecyclerView.Adapter<MoviesAdapter.MovieHolder>() {
+
+    var movies = listOf<Movie>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+        val view =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.film_view_holder, parent, false)
+                .apply { setOnClickListener(onMovieClick) }
+        return MovieHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+        val movie = movies[position]
+        glide.load(movie.posterPath).into(holder.poster)
+        holder.title.text = movie.title
+    }
+
+    class MovieHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val poster: ImageView = item.findViewById(R.id.poster)
+        val title: TextView = item.findViewById(R.id.title)
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("setDataSource")
+        fun setDataSource(view: RecyclerView, dataSource: LiveData<List<Movie>>) {
+            val adapter = view.adapter as MoviesAdapter
+            dataSource.observe(view.context as LifecycleOwner, Observer {
+                adapter.movies = it
+            })
+        }
+    }
+}

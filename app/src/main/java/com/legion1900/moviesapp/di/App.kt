01a1a14bd9ec6c27d281a9e530309f93161f893e
Legion1900 <rom4ik730@gmail.com>
2020-02-09 18:00:03 +0200

    Fixed backdrop image size. Implemented image size choosing through Dagger AppComponent setup.

--- app/src/main/java/com/legion1900/moviesapp/di/App.kt
+++ app/src/main/java/com/legion1900/moviesapp/di/App.kt
@@ -1,11 +1,15 @@
 package com.legion1900.moviesapp.di
 
 import android.app.Application
+import com.legion1900.moviesapp.data.impl.serialization.TMDBConfiguration
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        appComponent = DaggerAppComponent.create()
+        appComponent = DaggerAppComponent.builder()
+            .setPosterSize(TMDBConfiguration.Images.ImageSize.MEDIUM)
+            .setBackdropSize(TMDBConfiguration.Images.ImageSize.XLARGE)
+            .build()
     }
 
     companion object {

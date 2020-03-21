8724a1f15582b2ac880df7a70f734fbe2f278dc7
Legion1900 <rom4ik730@gmail.com>
2020-03-21 17:19:39 +0200

    Movie : Parcelable is now implemented through @Parcelize.

--- app/src/main/java/com/legion1900/moviesapp/domain/dto/Movie.kt
+++ app/src/main/java/com/legion1900/moviesapp/domain/dto/Movie.kt
@@ -1,11 +1,12 @@
 package com.legion1900.moviesapp.domain.dto
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 /*
-* Similar to Result, but contains no id and fully prepared poster links.
+* Similar to Result, but contains no id and has fully prepared poster links.
 * */
+@Parcelize
 data class Movie(
     val id: Int,
     val title: String,
@@ -16,42 +17,4 @@ data class Movie(
     val originalLanguage: String,
     val overview: String,
     val releaseDate: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readFloat(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(title)
-        parcel.writeString(originalTitle)
-        parcel.writeFloat(avgVote)
-        parcel.writeString(posterPath)
-        parcel.writeString(backdropPath)
-        parcel.writeString(originalLanguage)
-        parcel.writeString(overview)
-        parcel.writeString(releaseDate)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
